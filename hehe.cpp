0fd9d25820573537b2e75fd7d420805979ed428c
Lakhan Xettri  <itsmeyourcrush2061@gmail.com>
2024-09-18 10:42:28 +0545

    CPP CODE

---
+++ hehe.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int area(int br,int h){
+    return(0.5*br*h);
+}
+float area(int r){
+    return(3.14*r*r);
+}
+float area(float l, float br){
+    return(l*br);
+}
+int main( ){
+    // int b,h,r;
+    // float l,br,r;
+    // cout<<"enter height and breadth";
+    // cin>>b>>h;
+    // cout<<"area of traingle is:"<<area(b,h)<<endl;
+    // cout<<"enter radius";
+    // cin>>r;
+    // cout<<"area of circle is:"<<area(r)<<endl;
+    // cout<<"enter lenght and breadth";
+    // cin>>l>>br;n
+    // cout<<"area of rectangle is:"<<area(l,br)<<endl;
+    // return 0;
+
+    cout << "Hello, World!" << endl;
+}
